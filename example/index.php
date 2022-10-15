cac79bbf7daf3e36d6c79878e12ec0993ef8ac7a
Novath Thomas <57701433+pro-cms@users.noreply.github.com>
2022-10-15 17:06:05 +0300

    added zpay logo

--- example/index.php
+++ example/index.php
@@ -7,15 +7,15 @@ $api_key = "eae1a6ac-aa11-4af8-8a8a-68336de3b0cd";
 $api_secret = "eae1a6ac-aa11-4af8-8a8a-68336de3b0cd";
 $environment = "android";
 
-$zp = new ZepsonPay($api_key, $api_secret, $environment);
+$zp = new Zepson\ZepsonpaySDK\ZepsonPay($api_key, $api_secret, $environment);
  
 $data = [
-    // 'amount' => 100,
-    // 'purpose' => 'purpose',
+    'amount' => 100,
+    'purpose' => 'purpose',
     'ext_trxn_reff' => '9JF681RJWOK',
-    // 'phone' => '0688950846',
-    // 'operator' => 'vodacom',
-    // 'device' => '00000000-0000-0000-6da8-08a4c30f4b97'
+    'phone' => '0688950846',
+    'operator' => 'vodacom',
+    'device' => '00000000-0000-0000-6da8-08a4c30f4b97'
 
 ];
 // $payment = $zp->makePayment($data);
